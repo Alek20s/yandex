8dd8adb3e15bee34cde2e30ae46cc1a75aec0f21
root <root@4pfg.l.time4vps.cloud>
2025-08-22 00:36:24 +0300

    white spaces

--- the_snake.py
+++ the_snake.py
@@ -42,7 +42,7 @@ clock = pygame.time.Clock()
 # Тут опишите все классы игры.
 class GameObject:
     def __init__(self) -> None:
-        self.position = (SCREEN_WIDTH //2, SCREEN_HEIGHT //2)
+        self.position = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
         self.body_color = None
     def draw(self):
         pass
