b4aff44a4fc66c3c378dd10da78e83adbf6509ec
root <root@4pfg.l.time4vps.cloud>
2025-08-22 23:16:02 +0300

    discr

--- the_snake.py
+++ the_snake.py
@@ -46,7 +46,8 @@ class GameObject:
     def __init__(self) -> None:
         self.position = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
         self.body_color = None
-#  ending
+    
+    """ending"""
     def draw(self):
         pass
 
